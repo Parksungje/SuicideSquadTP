7f42f14707204f5d13709f3da1fbe43ace2f3828
Parksungje <Parksungje0213@gmail.com>
2025-10-12 18:53:28 +0900

    feat : WeaponController

---
+++ Assets/SJ/Code/Combat/WeaponController.cs
@@ -0,0 +1,34 @@
+using Code.Agents;
+using Code.Animations;
+using System;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour, IComponent
+{
+    [SerializeField] private ParamSO fireParam;
+    private Agent _agent;
+    private IAgentAnimator _agentAnimator;
+
+    private bool _isAttacking;
+        
+    public void Initialize(Agent agent)
+    {
+        _agent = agent;
+    }
+
+    private void SetAttacking(bool isAttacking) => _isAttacking = isAttacking;
+
+    private void Update()
+    {
+        if(_isAttacking)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+        _agentAnimator.SetTrigger(fireParam);
+    }
+}
