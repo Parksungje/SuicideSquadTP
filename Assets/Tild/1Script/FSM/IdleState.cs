09c4e67402af754a9d62b629f8f61859539d92cd
Tild720 <gloveno123@gmail.com>
2025-10-13 11:35:18 +0900

    Add : fsm base

---
+++ Assets/Tild/1Script/FSM/IdleState.cs
@@ -0,0 +1,24 @@
+﻿namespace Tild.FSM
+{
+    public class IdleState : State
+    {
+        public IdleState(Boss boss) : base(boss)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            
+        }
+
+        public override void OnUpdate()
+        {
+            
+        }
+
+        public override void OnExit()
+        {
+           
+        }
+    }
+}
