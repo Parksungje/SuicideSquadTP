20dacb0334d40260b70532dea61b7365aecea02c
Parksungje <Parksungje0213@gmail.com>
2025-10-17 22:25:25 +0900

    fix : PK MiniGame

--- Assets/SJ/Code/PK/PenlatyController.cs
+++ Assets/SJ/Code/PK/PenlatyController.cs
@@ -10,23 +10,31 @@ namespace Code.PK
         [SerializeField] private GameObject ballPrefab;
         [SerializeField] private Transform shootPoint;
         [SerializeField] private float shootForce = 100f;
+        [SerializeField] private GameObject _keeperPrefab;
+        [SerializeField] private TextMeshProUGUI _scoreText;
 
         private Rigidbody _rbCompo;
-        private Vector3 _shooterDir = Vector3.zero;
-        private Vector3 _keeperDir = Vector3.zero;
         private GameObject currentBall;
-
-        [SerializeField] private GameObject _keeperPrefab;
-
         private Vector3 _originPos;
 
         private bool _sConfirm = false;
         private bool _kConfirm = false;
 
-        [SerializeField] private TextMeshProUGUI _scoreText;
-
         private int _sScore = 0;
         private int _kScore = 0;
+
+        private enum Direction
+        {
+            Left,
+            Middle,
+            Right
+        }
+
+        private Direction _shooterDirType = Direction.Middle;
+        private Direction _keeperDirType = Direction.Middle;
+
+        private Vector3 _shooterDir = Vector3.forward;
+        private Vector3 _keeperDir = Vector3.zero;
 
         private void OnEnable()
         {
@@ -61,7 +69,6 @@ namespace Code.PK
         private void Start()
         {
             _originPos = _keeperPrefab.transform.position;
-
             ScoreUpdate();
             StatusInit();
         }
@@ -70,121 +77,88 @@ namespace Code.PK
         {
             _sConfirm = false;
             _kConfirm = false;
-
-            Logggggg();
-        }
-
-        private void HandleSConfrim()
-        {
-            _sConfirm = true;
-            Logggggg();
-        }
-
-        private void HandleSLeftDir()
-        {
-            _shooterDir = Vector3.left * 0.5f;
-        }
-
-        private void HandleSRightDir()
-        {
-            _shooterDir = Vector3.right * 0.5f;
-        }
-
-        private void HandleSMiddleDir()
-        {
-            _shooterDir = Vector3.zero;
-        }
-
-        private void HandleKConfrim()
-        {
-            _kConfirm = true;
-            Logggggg();
-        }
-
-        private void HandleKLeftDir()
-        {
-            _keeperDir = new Vector3(-6, _keeperPrefab.transform.position.y, _keeperPrefab.transform.position.z);
-        }
-
-        private void HandleKRightDir()
-        {
-            _keeperDir = new Vector3(6, _keeperPrefab.transform.position.y, _keeperPrefab.transform.position.z);
-        }
-
-        private void HandleKMiddleDir()
-        {
-            _keeperDir = _originPos;
-        }
-
-        private void HandleComfirm()
-        {
-            if (_sConfirm == false || _kConfirm == false)
-            {
-                Logggggg();
-                Debug.Log("누군가가 완료를 안함");
-                return;
-            }
-
-            // Shooter confirm
-            Debug.Log("슈터 확정 - 공 발사");
-
-            if (currentBall != null)
-                Destroy(currentBall);
-
-            currentBall = Instantiate(ballPrefab, shootPoint.position, Quaternion.identity);
-
-            Vector3 shooterDir = Vector3.forward;
-            shooterDir += _shooterDir;
-            shooterDir = shooterDir.normalized;
-
-            _rbCompo = currentBall.GetComponent<Rigidbody>();
-            if (_rbCompo != null)
-            {
-                _rbCompo.linearVelocity = Vector3.zero;
-                _rbCompo.angularVelocity = Vector3.zero;
-                _rbCompo.AddForce(shooterDir * shootForce, ForceMode.VelocityChange);
-            }
-
-            // Keeper confirm
-            Vector3 keeperDir = _keeperDir;
-
-            _keeperPrefab.transform.position = keeperDir;
-
-            WhoIsWinner();
-
-            StatusInit();
-        }
-
-        private void WhoIsWinner()
-        {
-            // todo: 내일 고칠거. 이거 confirm으로 하지 말고 방향이 일치한지 확인하려고 했던거임!!
-            if (_sConfirm != _kConfirm)
-                ScoreUpdate(true);
-            else
-                ScoreUpdate(false);
         }
 
         private void ScoreUpdate()
         {
             _sScore = 0;
             _kScore = 0;
-
             _scoreText.text = $"{_sScore} : {_kScore}";
         }
 
         private void ScoreUpdate(bool isShooterWin)
         {
-            if (isShooterWin)
-                _sScore += 1;
-            else
-                _kScore += 1;
-
+            if (isShooterWin) _sScore++;
+            else _kScore++;
             _scoreText.text = $"{_sScore} : {_kScore}";
         }
 
-        private void Logggggg()
+        private void HandleSConfrim() => _sConfirm = true;
+
+        private void HandleSLeftDir()
         {
-            //Debug.Log($"<b>Status Init!</b> <size=17><color=red>Shooter confirm: {_sConfirm}</color>, <color=blue>Keeper confirm: {_kConfirm}</color></size>");
+            _shooterDirType = Direction.Left;
+            _shooterDir = new Vector3(-0.5f, 0f, 1f);
+        }
+
+        private void HandleSRightDir()
+        {
+            _shooterDirType = Direction.Right;
+            _shooterDir = new Vector3(0.5f, 0f, 1f);
+        }
+
+        private void HandleSMiddleDir()
+        {
+            _shooterDirType = Direction.Middle;
+            _shooterDir = Vector3.forward;
+        }
+
+        private void HandleKConfrim() => _kConfirm = true;
+
+        private void HandleKLeftDir()
+        {
+            _keeperDirType = Direction.Left;
+            _keeperDir = _originPos + Vector3.left * 3f;
+        }
+
+        private void HandleKRightDir()
+        {
+            _keeperDirType = Direction.Right;
+            _keeperDir = _originPos + Vector3.right * 3f;
+        }
+
+        private void HandleKMiddleDir()
+        {
+            _keeperDirType = Direction.Middle;
+            _keeperDir = _originPos;
+        }
+
+        private void HandleComfirm()
+        {
+            if (!_sConfirm || !_kConfirm)
+                return;
+
+            if (currentBall != null)
+                Destroy(currentBall);
+
+            currentBall = Instantiate(ballPrefab, shootPoint.position, Quaternion.identity);
+            _rbCompo = currentBall.GetComponent<Rigidbody>();
+
+            var dir = _shooterDir.normalized;
+            _rbCompo.linearVelocity = Vector3.zero;
+            _rbCompo.angularVelocity = Vector3.zero;
+            _rbCompo.AddForce(dir * shootForce, ForceMode.VelocityChange);
+
+            _keeperPrefab.transform.position = _keeperDir;
+
+            WhoIsWinner();
+            StatusInit();
+        }
+
+        private void WhoIsWinner()
+        {
+            bool shooterWin = _shooterDirType != _keeperDirType;
+            ScoreUpdate(shooterWin);
         }
     }
 }
