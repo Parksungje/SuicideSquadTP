70d081c75dc3292172241bedc3a7e41edccb9695
8sehyun <8sehyun@gmail.com>
2025-10-19 18:14:18 +0900

    TagGame WIP 1

---
+++ Assets/SH/02.Scripts/GameManagers/GameManager_Tag.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameManager_Tag : MonoBehaviour
+{
+    public int scoreA { private set; get; } = 0;
+    public int scoreB { private set; get; } = 0;
+    public int time { private set; get; } = 120;
+}
