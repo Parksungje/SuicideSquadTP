8219b3dbbeef1d3e00733747d728e7119f517cbb
Tild720 <gloveno123@gmail.com>
2025-10-20 21:27:20 +0900

    Add : ROPE GAME

---
+++ Assets/Tild/1Script/Minigames/Rope/RopeInputSO.cs
@@ -0,0 +1,11 @@
+﻿using Code.Player;
+using UnityEngine;
+
+namespace Tild._1Script.Minigames.Rope
+{
+    [CreateAssetMenu(fileName = "Rope Input", menuName = "Inputs/Rope", order = 1)]
+    public class RopeInputSO : BaseInputSO
+    {
+        
+    }
+}
