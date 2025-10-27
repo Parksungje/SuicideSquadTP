79b17cf7242b954363aa988cd397bbc00954fe38
Parksungje <Parksungje0213@gmail.com>
2025-10-27 23:19:17 +0900

    fix : Running

--- Assets/SJ/01.Code/01.MiniGames/RunningSurvival/RunningGameManager.cs
+++ Assets/SJ/01.Code/01.MiniGames/RunningSurvival/RunningGameManager.cs
@@ -1,12 +1,16 @@
 using Code.Player;
 using System;
 using UnityEngine;
+using DG.Tweening;
+using System.Security.Cryptography;
 
 public class RunningGameManager : MonoBehaviour
 {
     [field: SerializeField] private RunningGameSO _runningInput;
+    [SerializeField] private GameObject _p1Obj;
+    [SerializeField] private GameObject _p2Obj;
 
-    private bool _aPressed, _dPressed, _upArrowPressed, _downArrowPressed;
+    private bool _aPressed, _dPressed, _leftArrowPressed, _rightArrowPressed;
 
     private void OnEnable()
     {
@@ -19,23 +23,43 @@ public class RunningGameManager : MonoBehaviour
         _runningInput.OnRightArrowDown += SetP2R;
     }
 
-    private void SetP1A(bool obj)
+    private void OnDestroy()
     {
-        throw new NotImplementedException();
+        if (_runningInput == null) return;
+
+        _runningInput.OnAKeyDown -= SetP1A;
+        _runningInput.OnDKeyDown -= SetP1D;
+
+        _runningInput.OnLeftArrowDown -= SetP2L;
+        _runningInput.OnRightArrowDown -= SetP2R;
     }
 
-    private void SetP1D(bool obj)
+    private void SetP1A(bool isPressed)
     {
-        throw new NotImplementedException();
+        _aPressed = isPressed;
     }
 
-    private void SetP2L(bool obj)
+    private void SetP1D(bool isPressed) 
     {
-        throw new NotImplementedException();
+        _dPressed = isPressed;
     }
 
-    private void SetP2R(bool obj)
+    private void SetP2L(bool isPressed)
     {
-        throw new NotImplementedException();
+        _leftArrowPressed = isPressed;
+    }
+
+    private void SetP2R(bool isPressed)
+    {
+        _rightArrowPressed = isPressed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_aPressed) _p1Obj.transform.DOMove(new Vector3(-16, 0, 15), 0.3f);
+        if (_dPressed) _p1Obj.transform.DOMove(new Vector3(-4, 0, 15), 0.3f);
+
+        if (_leftArrowPressed) _p2Obj.transform.DOMove(new Vector3(16, 0, 15), 0.3f);
+        if (_rightArrowPressed) _p2Obj.transform.DOMove(new Vector3(4, 0, 15), 0.3f);
     }
 }
