1d57d98123f1f04cff3f9df9b1e6dc658fd732f6
Tild720 <gloveno123@gmail.com>
2025-11-02 02:21:07 +0900

    Add : tutorial to 7 games

---
+++ Assets/Tild/1Script/Minigames/GameStarter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tild.Minigames
+{
+    public class GameStarter : MonoBehaviour
+    {
+        [SerializeField] private GameObject manager;
+
+        [SerializeField] private CanvasGroup keyGuideGroup;
+        [SerializeField] private TMP_Text timeCountText;
+        [SerializeField] private TMP_Text infoText;
+        [SerializeField] private Image background;
+        IEnumerator Start()
+        {
+            keyGuideGroup.DOFade(1, 2);
+            yield return new WaitForSeconds(5);
+
+            keyGuideGroup.DOFade(0, 1);
+            yield return new WaitForSeconds(1);
+
+            infoText.DOFade(1, 0.5f);
+            yield return new WaitForSeconds(5);
+
+            infoText.DOFade(0, 0.5f);
+            for (int i = 3; i > 0; i--)
+            {
+                timeCountText.text = i.ToString();
+                yield return new WaitForSeconds(1);
+            }
+
+            timeCountText.text = "";
+            manager.SetActive(true);
+            background.DOFade(0, 0.2f);
+        }
+    }
+}
