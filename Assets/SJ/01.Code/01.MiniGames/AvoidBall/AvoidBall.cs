3f0e5f891ce83ae5efcaec71a06b3f7ae34b8096
Parksungje <Parksungje0213@gmail.com>
2025-11-02 02:09:19 +0900

    fix : Victory & Defeat myGames

--- Assets/SJ/01.Code/01.MiniGames/AvoidBall/AvoidBall.cs
+++ Assets/SJ/01.Code/01.MiniGames/AvoidBall/AvoidBall.cs
@@ -5,19 +5,25 @@ public class AvoidBall : MonoBehaviour
     private Rigidbody _rb;
 
     [SerializeField] private float moveSpeed = 150f;
+    [SerializeField] private float spinForce = 50f;
     private float knockbackForce = 100f;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
-        _rb.freezeRotation = true;
+        _rb.freezeRotation = false;
     }
 
     private void Start()
     {
         Destroy(gameObject, 10f);
         _rb.linearVelocity = Vector3.back * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddTorque(Vector3.right * spinForce, ForceMode.Acceleration);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -32,15 +38,11 @@ public class AvoidBall : MonoBehaviour
                 Vector3 pushDirection = _rb.linearVelocity.normalized;
                 playerRb.AddForce(pushDirection * knockbackForce, ForceMode.Impulse);
             }
-
-            _rb.linearVelocity = Vector3.back * moveSpeed;
-            _rb.angularVelocity = Vector3.zero;
         }
     }
 
     private void OnCollisionStay(Collision collision)
     {
         _rb.linearVelocity = Vector3.back * moveSpeed;
-        _rb.angularVelocity = Vector3.zero;
     }
 }
